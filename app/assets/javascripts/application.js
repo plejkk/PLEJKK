20c54c108a2621d591c108acccf4af2585a13945
kentakasama <kasaken1113@gmail.com>
2019-06-03 09:35:33 +0000

    created slideDown action

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,23 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+$(function() {
+  // FAQのアコーディオン
+  $('.faq-list-item').click(function() {
+    var $answer = $(this).find('.answer');
+    if($answer.hasClass('open')) {
+      $answer.removeClass('open');
+      // slideUpメソッドを用いて、$answerを隠してください
+      // slideUpメソッドを用いて、$answerを隠してください
+      $answer.slideUp();
+      // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
+      $(this).find('span').text('+');
+    } else {
+      $answer.addClass('open');
+      // slideDownメソッドを用いて、$answerを表示してください
+      $answer.slideDown();
+      // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
+      $(this).find('span').text('-');
+    }
+  });
+});
