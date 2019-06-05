8e47b8482ea5deed2d3a6875575c08c8fde89e22
kentakasama <kasaken1113@gmail.com>
2019-06-05 11:00:54 +0000

    tried to set up destroy action in comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
   def create
+    # binding.pry
     joke = Joke.find(params[:joke_id])
     comment = Comment.new(joke_comment_params)
     comment.joke_id = joke.id
@@ -10,6 +11,15 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    binding.pry
+    # joke_idは送られてきている。
+    # しかし、comment_idがnilになっている
+    # したがってcomment_idを特定して削除することができなくなっている。
+    joke = Joke.find(params[:joke_id])
+    # comment = Comment.find(params[:comment_id])
+    comment = current_user.comments.find_by(comment_id: comment.id)
+    comment.destroy
+    redirect_to joke_path(params[:joke_id])
   end
 end
 private
