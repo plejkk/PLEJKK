80666135889b7747ef58a8c35fb64843158d8753
kentakasama <kasaken1113@gmail.com>
2019-06-06 03:56:29 +0000

    modified comment delete function

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :users, :only => [:index, :show, :edit, :update, :destroy]
   resources :jokes do
   	resource :favorites, only: [:create, :destroy]
-  	resource :comments, only: [:create, :destroy]
+  	resources :comments, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
